d07bfdfdb3624bd84eabf457cb27914bea9c7d01
gogotb81849 <gogotb@sina.com>
2026-07-29 04:45:13 +0800

    chore: add temp_backend_dist/ and ops-monitor/static/ to .gitignore, remove build artifacts

--- temp_backend_dist/diary/dto/create-diary.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateDiaryDto = void 0;
-var CreateDiaryDto = /** @class */ (function () {
-    function CreateDiaryDto() {
-    }
-    return CreateDiaryDto;
-}());
-exports.CreateDiaryDto = CreateDiaryDto;
--- temp_backend_dist/diary/dto/update-diary.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateDiaryDto = void 0;
-var UpdateDiaryDto = /** @class */ (function () {
-    function UpdateDiaryDto() {
-    }
-    return UpdateDiaryDto;
-}());
-exports.UpdateDiaryDto = UpdateDiaryDto;
